6540feebb7714e0b424172b496054afcade24313
Delevo <43750648+DelevoXDG@users.noreply.github.com>
2022-04-06 10:41:19 +0300

    P03-INF-ONP (#2)

---
+++ homework/ht5/TwoWay.java
@@ -0,0 +1,6 @@
+// zaimplementuj dwukierunkową listę cykliczną z iteratorem. Ma ona umożliwiać dodawanie, usuwanie i wyświetlanie kolejnych elementów cyklicznie i w obie strony. Zaprezentuj jej działanie na przykładzie, w tym fakt, że iterator działa cyklicznie i
+// można go przesuwać w obie strony.
+
+public class TwoWay {
+
+}

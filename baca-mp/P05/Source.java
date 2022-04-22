06bf6cf86727e20c2b26e274c2d6fc2695e52b14
Delevo <max.on777c@gmail.com>
2022-04-22 21:56:30 +0200

    recursive works

--- baca-mp/P05/Source.java
+++ baca-mp/P05/Source.java
@@ -27,37 +27,47 @@ class Backpack {
 			System.out.println(_weights[i]);
 		}
 	}
-
-	public void rec_pakuj(int requiredCapacity, int i, StringBuilder sb) {
-
-		if (found == true) {
-			return;
-		}
-		if (i >= _weights.length) {
-			return;			// Brak elementow, liczba 
-		}
-		requiredCapacity -= _weights[i];
+	public static int numLength(int n) {
+		if (n < 10) return 1;
+		if (n < 100) return 2;
+		if (n < 1000) return 3;
+		if (n < 10000) return 4;
+		if (n < 100000) return 5;
+		return 6;
+		// throw new IllegalArgumentException("num should be 10^6");
+	}
+	public boolean rec_pakuj(int requiredCapacity, int i, StringBuilder sb) {
+		String	temp		= sb.toString();
+		int		oldlength	= sb.length();
+		// if (found == true) {
+		// 	return;
+		// }
 		if (requiredCapacity == 0) {
-			sb.append(_weights[i]).append(" ");
 			found = true;
-			return;
+			return true;
 		}
 		if (requiredCapacity < 0) {
-			requiredCapacity += _weights[i];
-
-		} else {
-			sb.append(" ").append(_weights[i]);
+			return false;
+			// sb.append(" ").append(_weights[i]);
+			// rec_pakuj_old(requiredCapacity, i + 1, sb);
 		}
-		// if (requiredCapacity > 0)
-		rec_pakuj(requiredCapacity, i + 1, sb);
-
-		if (found == false) {
-			// if (requiredCapacity > 0) {
-			// 	sb.setLength(sb.length() - 2);
-			// }
-			rec_pakuj(this._capacity, i + 1, sb);
+		if (i >= _weights.length) {
+			return false;			// Brak elementow, liczba 
 		}
-		return;
+		requiredCapacity -= _weights[i];
+		sb.append(" ").append(_weights[i]);
+		boolean check = rec_pakuj(requiredCapacity, i + 1, sb);
+		if (check == true) {
+			return true;
+		}
+		requiredCapacity += _weights[i];
+		int newlength = sb.length();
+		sb.setLength(newlength - 1 - numLength(_weights[i]));	// Usuwanie spacji i dodanej wczesniej liczby
+		check = rec_pakuj(requiredCapacity, i + 1, sb);
+		if (check == true) {
+			return true;
+		}
+		return false;
 	}
 
 	public String PackWeight() {

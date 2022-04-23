161dd6ceb17c9feb48c4d982e1c7066fe4dc660a
Delevo <max.on777c@gmail.com>
2022-04-23 09:56:35 +0200

    misc

---
+++ homework/ht6/Factorial.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Factorial {
+	public static Scanner sc = new Scanner(System.in);
+
+	public static long Fact1(long num) {
+		if (num > 1)
+			return num * Fact1(num - 1);
+		return 1;
+	}
+
+	public static long Fact2(long n, long result) {
+		if (n == 0)
+			return result;
+		return Fact2(n - 1, result * n);
+	}
+
+	public static long Fact2(long n) {
+		return Fact2(n, 1);
+	}
+
+	public static void main(String[] args) {
+
+		long n = sc.nextLong();
+
+		System.out.println(Fact1(n));
+		System.out.println(Fact2(n));
+	}
+}
